fbdcf3a96bc0ca3875e1eadca36571c8983b80e7
lixiaohui <lixiaohui0812@gmail.com>
2018-07-24 16:42:09 +0800

    add abstraction of tasker

---
+++ task/interface.go
@@ -0,0 +1,28 @@
+package task
+
+import "context"
+
+type Tasker interface {
+	Name() string
+	Produce(ctx context.Context, msg interface{}) error
+	SetConsumer(fn func(ctx context.Context, msg interface{}))
+}
+
+type nopTasker struct{}
+
+func (*nopTasker) Name() string                                              { return "nop" }
+func (*nopTasker) Produce(ctx context.Context, msg interface{}) error        { return nil }
+func (*nopTasker) SetConsumer(fn func(ctx context.Context, msg interface{})) {}
+
+var taskMap = make(map[string]Tasker)
+
+func RegisterTasker(tasker Tasker) {
+	taskMap[tasker.Name()] = tasker
+}
+
+func GetTasker(name string) Tasker {
+	if t, ok := taskMap[name]; ok {
+		return t
+	}
+	return &nopTasker{}
+}
